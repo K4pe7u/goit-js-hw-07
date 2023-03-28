14a76f659e629c7eb723726d63894cd8782a3763
Juliusz Janicki <juliusz.janicki@gmail.com>
2023-03-28 21:22:15 +0200

    part 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,9 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+const createEl = document.createElement("div");
+console.log(createEl);
