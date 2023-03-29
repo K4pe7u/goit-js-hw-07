2f9671fb7125ed0de1eb569b9d048af65e39dde4
Juliusz Janicki <juliusz.janicki@gmail.com>
2023-03-29 20:20:00 +0200

    part 3
    working!

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,5 +5,31 @@ console.log(galleryItems);
 
 const galleryEl = document.querySelector(".gallery");
 console.log(galleryEl);
-const createEl = document.createElement("div");
-console.log(createEl);
+
+for (let item of galleryItems) {
+  const imageItem = document.createElement("li");
+  galleryEl.append(imageItem);
+
+  imageItem.insertAdjacentHTML(
+    "beforeend",
+    `<a class="gallery__link" href="${item.original}">
+  <img
+  class="gallery__image"
+  src="${item.preview}"
+  data-source="${item.original}"
+  alt="${item.description}"
+/>
+</a>`
+  );
+}
+
+galleryEl.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}"/>`,
+    {}
+  );
+
+  instance.show();
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+// for (const item of galleryItems) {}
+
+galleryItems.forEach((item) => {
+  const imageItem = document.createElement("li");
+  galleryEl.append(imageItem);
+
+  imageItem.insertAdjacentHTML(
+    "beforeend",
+    `<a class="gallery__link" href="${item.original}">
+  <img
+  class="gallery__image"
+  src="${item.preview}"
+  data-source="${item.original}"
+  alt="${item.description}"
+/>
+</a>`
+  );
+});
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 200,
+});
+
+// galleryEl.innerHTML = "";
+
+// galleryEl.innerHTML += newImage;
